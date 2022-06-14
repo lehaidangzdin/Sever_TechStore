617123fad7b6d6e2c7222539126b2eca0e700403
Lê Hải Đăng <lehaidangzdin123@gmail.com>
2022-06-15 00:27:43 +0700

    update API 5

--- routes/index.js
+++ routes/index.js
@@ -512,7 +512,14 @@ router.post("/getHoaDonAPI", function (req, res, next) {
                 message: err.message
             })
         } else {
-            return res.send(rows)
+            if (rows.length > 0) {
+                return res.send(rows)
+            } else {
+                return res.send({
+                    status: "failure",
+                    message: ""
+                })
+            }
         }
     })
 
