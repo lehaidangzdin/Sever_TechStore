683b3cc4e23a952c09bfbb49b68b7feb50fbd4c3
Lê Hải Đăng <79364337+lehaidangzdin@users.noreply.github.com>
2022-07-01 19:26:53 +0700

    Update index.js
    change config db

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,9 @@ router.use(cookieParser())
 router.use('/static', express.static('../public/stylesheets/style.css'))
 const con = mysql.createConnection({
     host: "sql6.freesqldatabase.com",
-    user: "sql6501908",
-    password: "VinVA1b1b2",
-    database: "sql6501908",
+    user: "sql6503347",
+    password: "wjMXZ19Pyf",
+    database: "sql6503347",
 });
 con.connect(function (err) {
     if (err) {
